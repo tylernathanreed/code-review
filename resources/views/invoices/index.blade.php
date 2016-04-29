d114bc01bdb981a8b6c2f8a35c29464b7d37e07d
Tyler Reed <tylernathanreed@gmail.com>
2016-04-29 10:25:15 -0500

    ~ Moved Content to Header

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
-@section('content')
-	Index Page
+@section('header')
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Invoices</h1>
+			<p>This is a listing of the invoices on the site.</p>
+			<p>
+				<a class="btn btn-primary btn-lg" href="{{ route('invoices.create') }}" role="button">
+					<i class="glyphicon glyphicon-plus"></i>
+					<span>Create Invoice</span>
+				</a>
+			</p>
+		</div> <!-- </container> -->
+	</div> <!-- </jumbotron> -->
 @endsection

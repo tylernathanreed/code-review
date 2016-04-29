986bb01df40455738f1cd348aa9130e71fc148e1
Tyler Reed <tylernathanreed@gmail.com>
2016-04-29 09:09:17 -0500

    + Created Invoice Lines Table

---
+++ database/migrations/2016_04_29_140317_create_invoice_lines_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/*
+|--------------------------------------------------------------------------
+| Invoice Lines Table
+|--------------------------------------------------------------------------
+|
+| The Invoice Lines Table records each Line of a specified Invoice, which
+| includes a description, an hourly price, and a number of hours. All
+| other aspects of Invoices can be calculated from this information.
+|
+*/
+class CreateInvoiceLinesTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create the Invoice Lines Table
+		Schema::create('invoice_lines', function (Blueprint $table)
+		{
+			// Identification
+			$table->integer('invoice_id')->unsigned()->primary();
+			$table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+
+			$table->integer('order')->unsigned();
+
+			// Attributes
+			$table->string('description', 100);
+			$table->integer('hourly_price')->unsigned();
+			$table->integer('hours')->unsigned();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop the Invoice Lines Table
+		Schema::drop('invoices');
+	}
+}

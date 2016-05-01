e4e27de381066a5bbd28ef0a9c5d878e2d31ac1f
Tyler Reed <tylernathanreed@gmail.com>
2016-04-30 22:30:56 -0500

    + Added Invoice and Invoice Line Models

---
+++ app/Models/Invoice.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Support\ValueObjects\Currency;
+
+/*
+|--------------------------------------------------------------------------
+| Invoice Model
+|--------------------------------------------------------------------------
+|
+| The Invoice Model represents an Entity stored by the Invoices Table.
+| Invoices contain financial information, and can contain many lines
+| which partain to it. Note that all currency is stored in cents.
+|
+*/
+class Invoice extends Model
+{
+	//////////////////
+	//* Attributes *//
+	//////////////////
+	/**
+	 * The Attributes that a Protected from Mass Assignment.
+	 *
+	 * @var array
+	 */
+	protected $guarded = ['id'];
+
+	/////////////////
+	//* Accessors *//
+	/////////////////
+	/**
+	 * Calculates the Subtotal of the Lines.
+	 *
+	 * @return Currency
+	 */
+	public function subtotal()
+	{
+		// Determine the Subtotal in Cents
+		$cents = $this->lines->reduce(function($previous, $current) { return $previous + $current->total; }, 0);
+
+		// Convert the Subtotal to a Currency Object
+		return new Currency($cents);
+	}
+
+	/**
+	 * Returns the Taxes as a Currency Object.
+	 *
+	 * @return Currency
+	 */
+	public function taxes()
+	{
+		return new Currency($this->attributes['taxes']);
+	}
+
+	/**
+	 * Calculates the Total of the Lines.
+	 *
+	 * @return Currency
+	 */
+	public function total()
+	{
+		// Determine the Subtotal
+		$subtotal = $this->subtotal()->inCents();
+
+		// Add the Taxes to Determine the Total
+		$total = $subtotal + $this->taxes()->inCents();
+
+		// Convert the Total to a Currency Object
+		return new Currency($total);
+	}
+
+	/////////////////
+	//* Overrides *//
+	/////////////////
+	/**
+	 * Returns the Subtotal as a String-Formatted Currency
+	 *
+	 * @return string
+	 */
+	public function getSubtotalAttribute()
+	{
+		return $this->subtotal()->asCurrency();
+	}
+
+	/**
+	 * Returns the Taxes as a String-Formatted Currency
+	 *
+	 * @return string
+	 */
+	public function getTaxesAttribute()
+	{
+		return $this->taxes()->asCurrency();
+	}
+
+	/**
+	 * Returns the Total as a String-Formatted Currency
+	 *
+	 * @return string
+	 */
+	public function getTotalAttribute()
+	{
+		return $this->total()->asCurrency();
+	}
+
+	/////////////////////
+	//* Relationships *//
+	/////////////////////
+	/**
+	 * Returns the Invoice Lines that belong to this Invoice.
+	 *
+	 * @return Relationship
+	 */
+	public function lines()
+	{
+		return $this->hasMany(InvoiceLine::class, 'invoice_id', 'id')->orderBy('order');
+	}
+}
---
+++ app/Models/InvoiceLine.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Support\ValueObjects\Currency;
+
+/*
+|--------------------------------------------------------------------------
+| Invoice Line Model
+|--------------------------------------------------------------------------
+|
+| The Invoice Line Model represents an Entity stored by the Invoice Lines
+| Table. These Lines pertain to a parent Invoice, and contain financial
+| information. Note that all currency is stored in the form of cents.
+|
+*/
+class InvoiceLine extends Model
+{
+	//////////////////
+	//* Attributes *//
+	//////////////////
+	/**
+	 * The Attributes that can be Mass Assignment.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['order', 'description', 'hourly_price', 'hours'];
+
+	/**
+	 * Whether or not this Model uses 'created_at' and 'updated_at' Timestamps.
+	 *
+	 * @var boolean
+	 */
+	public $timestamps = false;
+
+	/////////////////
+	//* Accessors *//
+	/////////////////
+	/**
+	 * Returns the Hourly Price as a Currency Object.
+	 *
+	 * @return Currency
+	 */
+	public function price()
+	{
+		return new Currency($this->price);
+	}
+
+	/**
+	 * Returns the Total Coust as a Currency Object.
+	 *
+	 * @return Currency
+	 */
+	public function total()
+	{
+		return new Currency($this->total);
+	}
+
+	/////////////////
+	//* Overrides *//
+	/////////////////
+	/**
+	 * Returns the Hourly Price (in Cents).
+	 *
+	 * @return integer
+	 */
+	public function getPriceAttribute()
+	{
+		return $this->hourly_price;
+	}
+
+	/**
+	 * Returns the Total Cost of the Invoice Line.
+	 *
+	 * @return integer
+	 */
+	public function getTotalAttribute()
+	{
+		return $this->price * $this->hours;
+	}
+
+	/////////////////////
+	//* Relationships *//
+	/////////////////////
+	/**
+	 * Returns the Invoice that owns this Invoice Line.
+	 *
+	 * @return Relationship
+	 */
+	public function invoice()
+	{
+		return $this->belongsTo(Invoice::class, 'invoice_id');
+	}
+}

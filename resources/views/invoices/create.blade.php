14ed529002e5211b6a5862d7b7d3321099cca13a
Tyler Reed <tylernathanreed@gmail.com>
2016-04-30 22:27:05 -0500

    ~ Finished basic creation form

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -28,59 +28,106 @@
 				<div class="container-fluid">
 					<div class="row">
 						<div class="col-xs-12 col-sm-6">
-							<div class="form-group">
+							<div class="form-group{{ has_error('bill-to') }}">
 								<label for="bill-to" class="form-label">Bill To:</label>
-								<textarea name="bill-to" class="form-control" placeholder="1234 Main Street" style="max-height: 7em"></textarea>
+								<textarea name="bill-to" class="form-control" placeholder="1234 Main Street" style="max-height: 7em">{{ old('bill-to') }}</textarea>
+								@error('bill-to')
 							</div> <!-- </form-group> -->
 						</div> <!-- </col> -->
 
 						<div class="col-xs-12 col-sm-6">
 							<div class="form-group">
 								<label for="date" class="form-label">Date:</label>
-								<input type="date" name="date"/ class="form-control">
+								<input type="date" name="date" class="form-control" value="{{ old('date', date('Y-m-d')) }}">
 							</div> <!-- </form-group> -->
 						</div> <!-- </col> -->
 					</div> <!-- </row> -->
 
-					<table class="table --layout-fixed">
-						<tbody>
-							<tr>
-								<th class="--center">Description</th>
-								<th class="--center">Hourly Price</th>
-								<th class="--center">Hours</th>
-								<th class="--center">Total</th>
-							</tr>
-							<tr>
-								<td>
-									<input type="text" name="description[]" placeholder="Description" class="form-control"/>
-								</td>
-								<td>
-									<div class="input-group">
-										<span class="input-group-addon">$</span>
-										<input type="number" name="price[]" class="form-control"/>
-									</div> <!-- </input-group> -->
-								</td>
-								<td>
-									<input type="number" name="hours[]" class="form-control"/>
-								</td>
-								<td>
-									<div class="input-group">
-										<span class="input-group-addon">$</span>
-										<input type="number" class="form-control" readonly/>
-									</div> <!-- </input-group> -->
-								</td>
-							</tr>
-							<tr>
-								<th colspan="3" class="--right">Subtotal</tH>
-								<td>
-									<div class="input-group">
-										<span class="input-group-addon">$</span>
-										<input type="number" class="form-control" readonly/>
-									</div> <!-- </input-group> -->
-								</td>
-							</tr>
-						</tbody>
-					</table>
+					<div class="container-flex">
+						<div class="flex-row --padded-horizontal">
+							<div class="flex-col-xs-1 flex-col-sm-1 flex-col-md-2 --center"><b>Description</b></div>
+							<div class="flex-col-xs-1 --center"><b>Hourly Price</b></div>
+							<div class="flex-col-xs-1 --center"><b>Hours</b></div>
+							<div class="flex-col-xs-1 --center"><b>Total</b></div>
+						</div> <!-- </flex-row> -->
+
+						@foreach([0, 1, 2, 3] as $index)
+							<div class="flex-row">
+								<i class="glyphicon glyphicon-move"></i>
+
+								<div class="container-flex-xs flex-row-sm --flush --padded-horizontal-xs">
+									<div class="flex-row-xs-1 flex-col-sm-1 flex-col-md-2{{ has_error("line.$index.description") }}">
+										<input type="text" name="line[{{ $index }}][description]" placeholder="Description" class="form-control" value="{{ old("line.$index.description") }}"/>
+										@error("line.$index.description")
+									</div> <!-- </flex-col> -->
+
+									<div class="flex-row-xs-1 flex-col-sm-1{{ has_error("line.$index.price") }}">
+										<div class="input-group">
+											<span class="input-group-addon">$</span>
+											<input type="number" step="0.01" name="line[{{ $index }}][price]" placeholder="Hourly Price" min="0" class="form-control" value="{{ old("line.$index.price", 0) }}"/>
+										</div> <!-- </input-group> -->
+										@error("line.$index.price")
+									</div> <!-- </flex-col> -->
+
+									<div class="flex-row-xs-1 flex-col-sm-1{{ has_error("line.$index.hours") }}">
+										<input type="number" step="0.01" name="line[{{ $index }}][hours]" placeholder="Hours" min="0" class="form-control" value="{{ old("line.$index.hours", 0) }}"/>
+										@error("line.$index.hours")
+									</div> <!-- </flex-col> -->
+
+									<div class="flex-row-xs-1 flex-col-sm-1">
+										<div class="input-group">
+											<span class="input-group-addon">$</span>
+											<input type="number" step="0.01" name="line[{{ $index }}][total]" placeholder="Total" min="0" class="form-control" readonly/>
+										</div> <!-- </input-group> -->
+									</div> <!-- </flex-col> -->
+
+									<input type="hidden" name="line[{{ $index }}][order]" value="{{ $index }}"/>
+								</div> <!-- </flex-row> -->
+
+								<i class="glyphicon glyphicon-trash"></i>
+							</div> <!-- </flex-row> -->
+						@endforeach
+
+						<div class="flex-row">
+							<div class="flex-col-xs-1 --center">
+								<a class="btn btn-primary --half-width" href="#" role="button">
+									<i class="glyphicon glyphicon-plus"></i>
+									<span>Add New Line</span>
+								</a>
+							</div>
+						</div> <!-- </flex-row> -->
+
+						<div class="flex-row --padded-horizontal">
+							<div class="flex-col-xs-1 flex-col-sm-7 --right"><b>Subtotal</b></div>
+							<div class="flex-col-xs-6 flex-col-sm-2">
+								<div class="input-group">
+									<span class="input-group-addon">$</span>
+									<input type="number" step="0.01" name="subtotal" placeholder="Subtotal" min="0" class="form-control" readonly/>
+								</div> <!-- </input-group> -->
+							</div> <!-- </flex-col> -->
+						</div> <!-- </flex-row> -->
+
+						<div class="flex-row --padded-horizontal">
+							<div class="flex-col-xs-1 flex-col-sm-7 --right"><b>Taxes</b></div>
+							<div class="flex-col-xs-6 flex-col-sm-2">
+								<div class="input-group{{ has_error("taxes") }}">
+									<span class="input-group-addon">$</span>
+									<input type="number" step="0.01" name="taxes" placeholder="Taxes" min="0" class="form-control"/>
+								</div> <!-- </input-group> -->
+								@error("taxes")
+							</div> <!-- </flex-col> -->
+						</div> <!-- </flex-row> -->
+
+						<div class="flex-row --padded-horizontal">
+							<div class="flex-col-xs-1 flex-col-sm-7 --right"><b>Total</b></div>
+							<div class="flex-col-xs-6 flex-col-sm-2">
+								<div class="input-group">
+									<span class="input-group-addon">$</span>
+									<input type="number" step="0.01" name="total" placeholder="Total" min="0" class="form-control" readonly/>
+								</div> <!-- </input-group> -->
+							</div> <!-- </flex-col> -->
+						</div> <!-- </flex-row> -->
+					</div> <!-- </flex-container> -->
 
 					<button type="submit" class="btn btn-primary --full-width">
 						<i class="glyphicon glyphicon-plus"></i>

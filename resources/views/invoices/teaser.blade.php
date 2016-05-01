6bb755728589622ede88e99eb1b862b6f4c28566
Tyler Reed <tylernathanreed@gmail.com>
2016-04-30 22:28:33 -0500

    ~ Finished Teaser Page

--- resources/views/invoices/teaser.blade.php
+++ resources/views/invoices/teaser.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.app')
-
-@section('content')
-	Teaser Page
-@endsection
+<div class="flex-row --space-between">
+	<div>
+		<h4 class="list-group-item-heading">{{ $invoice->bill_to }}</h4>
+		<p class="list-group-item-text">
+			<b>Subtotal: </b>{{ $invoice->subtotal }}
+			<b>Taxes: </b>{{ $invoice->taxes }}
+			<b>Total: </b>{{ $invoice->total }}
+		</p>
+	</div>
+	<div>
+		<span class="--large --bold">{{ $invoice->lines->count() }}</span>
+		<span class="--faded">Lines</span>
+	</div>
+</div> <!-- </flex-row> -->
+<br>
+<a class="btn btn-primary" href="{{ route('invoices.show', $invoice->id) }}" role="button">View Details</a>

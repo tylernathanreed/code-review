9816ada5fe97675efe847237ddba9ee59db1e3a5
Tyler Reed <tylernathanreed@gmail.com>
2016-05-01 23:05:19 -0500

    + Added money_format Helper for Windows OS

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -20,3 +20,22 @@ if(!function_exists('has_error'))
 		return '';
 	}
 }
+
+if(!function_exists('money_format'))
+{
+	/**
+	 * Returns a Money Format representation of the specified Amount using the specfied Format.
+	 *
+	 * Note: This function already exists on Linus Operating Systems, and therefore this
+	 * helper will only be used on Windows Operating Systems (Or Mac).
+	 *
+	 * @param  string  $format  The specified Format.
+	 * @param  number  $amount  The specified Amount.
+	 *
+	 * @return string
+	 */
+	function money_format($format, $amount)
+	{
+		return sprintf('%01.2f', $amount);
+	}
+}
